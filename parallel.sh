6b79f218fae8b655890db5c446fdbba1c2ba3a2e
Yu Hu <nkhuyu@gmail.com>
2014-07-10 20:28:42 -0600

    #0: update parallel

--- parallel.sh
+++ parallel.sh
@@ -7,8 +7,29 @@ split -d -l $line $fileName $fileName.part
 
 for fl in $fileName.part*
 do
+echo "Process php test.php " \"$fl\" "has started: "
 php test.php $fl &
+pid=$!
+ PID_LIST+=" $pid";
 done
-cat output* > $fileName.output.csv
 
- 
+trap "kill $PID_LIST" SIGINT
+
+echo "Parallel processes have started";
+
+wait $PID_LIST
+
+echo
+echo "All processes have completed";
+
+echo "Merge files ... "
+cat output* > $fileName.output.csv
+echo "Finished Merge !"
+
+echo "Clean temp files ... "
+for fl in $fileName.part*
+do
+rm -rf $fl
+rm -rf output.$fl
+done
+echo "Finished clean files !"
