2f17a5f3bff9d93bb14a437e7e9b650b86294717
Yu Hu <nkhuyu@gmail.com>
2014-07-10 20:10:46 -0600

    #0: add a test php script

---
+++ test.php
@@ -0,0 +1,12 @@
+<?php
+echo $argv[1];
+
+$output = 'output'.$argv[1];
+// The new person to add to the file
+$person = "John Smith\n";
+// Write the contents to the file,
+// using the FILE_APPEND flag to append the content to the end of the file
+// and the LOCK_EX flag to prevent anyone else writing to the file at the same time
+file_put_contents($output, $person, FILE_APPEND | LOCK_EX);
+?>
+
